80b114efb46601a2b3bca9ca2db150652b9449b6
bnjma11973 <benjamin.montenegro04@inacapmail.cl>
2026-07-13 11:30:02 -0400

    añadido bootstrap a componentes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,11 @@ function Header()
 {
     return (
         <>
+            <div class="container-sm px-4">
+                <div class="h1">Landing page de un tema que esta por verse</div>
+                <div class="lead">asdkjnasd</div>
+            </div>
+            <hr></hr>
         </>
     )
 }
