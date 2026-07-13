fe821f9ee2c51911edd23dbe576138fcb0bd9730
bnjma11973 <benjamin.montenegro04@inacapmail.cl>
2026-07-13 12:50:58 -0400

    imagen del header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,39 @@
+///
+import miImagen from "../assets/headertest.png";
 
 function Header()
 {
     return (
         <>
-            <div class="container-sm px-4">
-                <div class="h1">Landing page de un tema que esta por verse</div>
-                <div class="lead">asdkjnasd</div>
+            <div class="container-fluid p-2">
+                <div class=" container 
+                position-relative
+                ">
+                    <div class="container
+                    d-inline-block
+                    ">
+                         {/* IMAGEN */}
+                        <img src={miImagen} class="img-fluid" alt="..."></img>
+                    </div>
+                    {/* TITULO Y SUBTITULO */}
+                    <div class=" container
+                    w-50
+                    position-absolute bottom-0 left-20
+                    bg-white
+                    border border-3 border-primary
+                    ">
+                        <p class="h2">
+                            Landing page de un tema que esta por verse...
+                        </p>
+                        <p class="lead">
+                            (la imagen es de prueba)
+                        </p>
+
+                    </div>
+                    <hr></hr>
+                </div>
             </div>
-            <hr></hr>
+            
         </>
     )
 }
